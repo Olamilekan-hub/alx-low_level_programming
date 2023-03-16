4b580d31610a24c9f7790026fed6ef635baaf158
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-16 13:44:50 -0700

    1st C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,13 @@
  * main - This is a 'void' function and als represents
  * the begining of the file.
  *
- * puts: this function prints out the char in the string()
+ * @puts: this function prints out the char in the string()
  *
- * Return - returns the value 0 for the main function
+ * @Return the value 0 for the main function
  */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
 
-	return 0;
+	return (0);
 }
