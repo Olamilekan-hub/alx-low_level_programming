b30e745f083e7227ea6b66bab0dae3ca5701995f
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-16 14:25:57 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/* main - Prints the value of printf
+/* 
+ * main - Prints a line of code using printf
  *
  * return: always 0 (success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	/* print a messsage to the standard output */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
