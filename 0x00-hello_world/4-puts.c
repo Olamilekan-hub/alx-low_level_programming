42346aeaf9d248cad035194640b00ec04d324454
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-16 13:06:46 -0700

    1st C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Function `main` is a 'void' function
+ * it returns int 0
+ *
+ * the function `puts` prints out the char in the string ()
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle.\n");
