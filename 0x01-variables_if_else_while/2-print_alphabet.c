2b631d50a35129c8dfcd535f2d061ccdc16d907d
FATUNBI ALIAMEEN (OLAMILEKAN) <adebukolaolamilekan123@gmail.com>
2023-03-17 23:01:10 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int A;
 	/**
-	 * while: this fucntion is used to loop the program till a condition is met
+	 * for: this fucntion is used to loop the program till a condition is met
 	 *
 	 * putchar is used to print a single char to the std output
 	 *
