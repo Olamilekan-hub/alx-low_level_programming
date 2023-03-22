b501ef9aacdf33e4847828b81c49d8f4f0c41cbd
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-21 19:26:40 -0700

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_to_98 - prints the integer from 0 to 98
+ * @n: starting number
+ *
+ * Return void
+ *
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
+}
