74d111e1ad631cbcb70e80dd3624b9ea76ad1e4c
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-27 22:01:27 +0000

    strtring length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen - prints the length of the string
+ * @s: char to store the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	char *p = s;
+	while (*p++)
+	{
+		i++;
+	}
+	return (i);
+}
