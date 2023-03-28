662447c0b984e13fcaae8c1ff6a9c841bc4366aa
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-28 05:19:24 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+/**
+ * _atoi - convert a string to an integer.
+ * @s: string to convert.
+ *
+ * Return: the integer value of the string, or 0 if no number is found.
+ */
+int _atoi(char *s)
+{
+	int i, sign = 1, res = 0;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			res = res * 10 + s[i] - '0';
+		}
+                else if (res > 0)
+		{
+			break;
+		}
+        }
+        return (sign * res);
+}
