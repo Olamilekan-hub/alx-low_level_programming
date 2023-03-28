5dd779fa495e903a2850bdf291b33db7ea25b09e
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-28 19:41:50 +0000

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,26 +12,17 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int res = 0;
+	unsigned int res = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 		{
-			sign = -1;
-		}
-		else if (s[i] == '+')
-		{
-			sign = +1;
+			sign *= -1;
 		}
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (res > INT_MAX / 10 ||
-					((res == INT_MAX / 10) && (s[i] - '0' > INT_MAX) & 10))
-			{
-				return (sign == 1 ? INT_MAX : INT_MIN);
-			}
-			res = res * 10 + s[i] - '0';
+			res = (res * 10) + (s[i] - '0');
 		}
 		else if (res > 0)
 		{
