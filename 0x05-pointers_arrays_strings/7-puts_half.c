32ea95ab4bd0964b04faf7faa29e97d4cb32d2cb
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-28 16:10:33 +0000

    other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,14 +4,14 @@
 #include "main.h"
 /**
  * puts_half - prints other half of a string
- * @str - stored string
+ * @str: stored string
  *
  */
 void puts_half(char *str)
 {
 	int i, g;
 	int len = strlen(str);
-	
+
 	if (len % 2 == 0)
 	{
 		i = len / 2;
