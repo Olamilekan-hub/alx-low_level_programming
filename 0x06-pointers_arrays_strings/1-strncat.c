df5bdf01d2b2acb4848f1c9ebd2a9d9b47b8d876
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-30 17:05:38 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strcat - copy strings from one pointer to another &&
+ * _strncat - copy strings from one pointer to another &&
  *		overwriting the teminating null byte (\0)
  * @src: source to copy from
  * @dest: copying destination
@@ -13,13 +13,12 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int a = 0, i = 0;
-		
 
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
-	for (i =0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[a + i] = src[i];
 	}
