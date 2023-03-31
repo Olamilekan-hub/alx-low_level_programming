36b974e43860463762361c6e3b434d3264cb63c0
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-03-31 05:20:24 +0000

    updating

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,48 @@
-#include <stdio.h>
 #include "main.h"
-/**
- * rot13 - substitute cipher
- * @s: string
- *
- * Return: s
- */
-char *rot13(char *s)
-{
-	char *p = s;
 
-	while (*p != '\0')
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
+
+char *rot13(char *str)
+{
+	int indx1 = 0, indx2;
+
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (str[indx1])
 	{
-		if (*p >= 'a' && *p <= 'z')
+		for (indx2 = 0; indx2 < 52; indx2++)
 		{
-			*p = ((*p - 'a') + 13) % 26 + 'a';
+			if (str[indx1] == alphabet[indx2])
+			{
+				str[indx1] = rot13key[indx2];
+				break;
+			}
 		}
-		else if (*p >= 'A' && *p <= 'Z')
-		{
-			*p = ((*p - 'A') + 13) % 26 + 'A';
-		}
-		p++;
+
+		indx1++;
 	}
-	return (s);
+
+	return (str);
 }
