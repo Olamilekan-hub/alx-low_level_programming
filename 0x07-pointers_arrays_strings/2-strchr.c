9b026e7580ea6731d931a9d97c2d7260f658ab33
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-04 01:30:03 +0000

    search for a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - this function locates a character in a string
+ * @s: pointer to the string to be searched
+ * @c: character to be seearched for in the string
+ *
+ * Return: NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+		{
+			return (char *)s;
+		}
+		s++;
+	}
+
+	return (s);
+}
