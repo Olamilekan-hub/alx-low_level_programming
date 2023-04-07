47ae52c4f637bc75d75ed03b00f2bdba53c214cc
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-07 10:43:37 -0700

    reverse recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,12 +7,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-	int len = strlen(s);
-	int i = len - 1;
-
-	while (i >= 0)
+	if (*s == '\0')
 	{
-		printf("%c", s[i]);
-		i--;
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
 }
