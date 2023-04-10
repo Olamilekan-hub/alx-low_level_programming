c1f27f345017fe6f537f384e01798dd463685d62
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-10 15:01:49 +0000

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - gives the power of an integer
+ * @x: intger to be multiply
+ * @y: integer to be raised
+ *
+ * return: value of the power an integer to an integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (1 / x * _pow_recursion(x, y + 1));
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
