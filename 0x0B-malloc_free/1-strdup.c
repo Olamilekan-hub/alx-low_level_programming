11ada46c0b43efcb73322ac6afee32f4ac3dfe4f
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-12 23:25:26 +0000

    string suplication with malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strdup - returns a pointer to a newly allocated copy of the string
+ * @str: string to be duplicated
+ *
+ * Return: the duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *dup = malloc(sizeof(str) + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dup, str);
+	return (dup);
+	free(dup);
+}
