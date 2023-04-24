a80815151cc9c7ecce83bdcd54ddc4027f9281f5
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-24 14:39:49 +0000

    updating

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -39,7 +39,7 @@ void print_all(const char * const format, ...)
 					printf("%s", p);
 					break;
 				}
-				printf("(nill)");
+				printf("(nil)");
 				break;
 		}
 		if (format[(i + 1)] != '\0' && (format[i] == 'c' || format[i] == 'i' ||
