1b51f04463c680e8752334a4ebd26b123d15317b
Olamilekan <adebukolaolamilekan123@gmail.com>
2023-04-24 00:43:10 +0000

    static library from shell script

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ar rcs libmy.a puts.o islower.o isalpha.o abs.o isupper.o isdigit.o strlen.o strcpy.o atoi.o strcat.o strncat.o strncpy.o strcmp.o memset.o memcpy.o strchr.o strspn.o strpbrk.o strstr.o putchar.o
