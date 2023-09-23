6d860f23f374415ef77deb8d5f04393e3ae9884d
FATUNBI ALIAMEEN (OLAMILEKAN) <adebukolaolamilekan123@gmail.com>
2023-09-23 11:43:02 +0100

    Add files via upload

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the standard output.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* Print a message to the standard output */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description:  Prints a line of code using printf
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* print a messsage to the standard output */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
